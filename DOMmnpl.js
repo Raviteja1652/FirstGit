5b54bd98d5fd1489432887fbebead403c2f63eee
Raviteja1652 <ravitejamrt.165@gmail.com>
2023-09-29 13:35:28 +0530

    more DOM manipulations

--- DOMmnpl.js
+++ DOMmnpl.js
@@ -1,4 +1,4 @@
-console.dir(document);
+/*console.dir(document);
 console.log(document.URL);
 console.log(document.title);
 console.log(document.head);
@@ -22,4 +22,49 @@ console.log(dif);
 dif[0].style.backgroundColor = 'red';
 var diff = document.getElementsByTagName('li');
 console.log(diff);
-diff[4].style.backgroundColor = 'blue';
+diff[4].style.backgroundColor = 'blue';*/
+
+var p = document.querySelector('#items');
+console.log(p.parentNode);
+console.log(p.parentElement);
+console.log(p.lastChild);
+console.log(p.lastElementChild);
+console.log(p.firstChild);
+console.log(p.firstElementChild);
+console.log(p.nextElementSibling);
+console.log(p.nextSibling);
+console.log(p.previousElementSibling);
+console.log(p.previousSibling);
+p.previousElementSibling.style.color = 'red';
+var newDiv = document.createElement('Div');
+console.log(newDiv);
+newDiv.setAttribute('title', 'erripuk');
+var newDivText = document.createTextNode('HEllo');
+newDiv.appendChild(newDivText);
+
+var c = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+c.insertBefore(newDiv, h1);
+
+console.log(p.firstElementChild);
+p.firstElementChild.textContent = 'HEllo item1';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
